ab5552f5c446ee12c9579989c2ebac29a28625a9
zhanxiao <zhanxiaolove@163.com>
2019-10-22 10:15:45 +0800

    任务项目放进单独的文件夹JobList，JobBase基类移到Core项目

--- JobTest002/JobTest002.cs
+++ JobTest002/JobTest002.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ZTPlanningTasksBase;
+using ZTPlanningTasksCore;
 
 namespace JobTest002
 {
---
+++ ZTPlanningTasksCore/JobBase.cs
@@ -0,0 +1,24 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTPlanningTasksCore
+{
+    public abstract class JobBase : MarshalByRefObject, IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            return Execute();
+        }
+
+        public abstract Task Execute();
+
+        public void WriteLog()
+        {
+
+        }
+    }
+}
